ff7c84024f4e90a70449b895a1a1c7aecfea44f8
mohit <mohit20.sikri@gmail.com>
2017-02-21 20:32:36 +0530

    //milk delivery app setup

---
+++ app/src/main/java/milkdelivery/mohit/com/mdapp/MainActivity.java
@@ -0,0 +1,13 @@
+package milkdelivery.mohit.com.mdapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
